f238e6ca1b84994c626359d82902fb9834c11d5b
johnlodev <nick781023@gmail.com>
2025-12-09 09:05:26 +0000

    update

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,7 @@ import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "./schema";
 
-// 建立與 Turso (LibSQL) 的連線客戶端
+// 建立與 Turso (LibSQL) 的連線客戶端的
 const client = createClient({
   url: process.env.TURSO_DATABASE_URL!,
   authToken: process.env.TURSO_AUTH_TOKEN,
